d1102c3485d7d3ab9898e5e8778e0ac381d8212b
Omegahub <omegahub2019@gmail.com>
2020-02-07 22:26:33 -0300

    Aula de Java1

---
+++ src/Nadador.java
@@ -0,0 +1,21 @@
+
+public class Nadador {
+	public static void main(String[] args) {
+	float idade = 19;
+		if (idade >= 5 && idade <= 7) {
+			System.out.println("Infantil");
+		} else if (idade >= 8 && idade <= 10) {
+			System.out.println("Juvenil");
+		}
+		else if (idade >= 11 && idade <= 15) {
+			System.out.println("Adolescente");
+		}
+		else if (idade >= 16 && idade <= 30) {
+			System.out.println("Adulto");
+		}
+		else if (idade >= 30) {
+			System.out.println("Sênior");
+		}
+//tentar com boolean
+}
+}
